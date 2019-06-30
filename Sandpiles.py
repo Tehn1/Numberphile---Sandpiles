0ed82fba1e2f5e8e07de6765a84c880b30ccce21
Tehn1 <eihseo@hotmail.com>
2019-06-30 11:37:55 +0100

    Update Sandpiles.py
    Added the cursor;
    Added colour changes;

--- Sandpiles.py
+++ Sandpiles.py
@@ -6,12 +6,24 @@ import numpy
 
 from PIL import Image
 
+from functools import partial
+
 class Ui_MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
 
         self.setupUi(self)
+
+
+    #Repurpose QLabel (eyllanesc)
+
+    class ClickLabel(QtWidgets.QLabel):
+        clicked = QtCore.pyqtSignal()
+
+        def mousePressEvent(self, event):
+            self.clicked.emit()
+            QtWidgets.QLabel.mousePressEvent(self, event)
 
     #QtDesigner Output
     def setupUi(self, MainWindow):
@@ -36,62 +48,62 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.Builder_Line.setFont(font)
         self.Builder_Line.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
         self.Builder_Line.setObjectName("Builder_Line")
-        self.Builder_1 = QtWidgets.QLabel(self.Builder_Group)
+        self.Builder_1 = Ui_MainWindow.ClickLabel(self.Builder_Group)
         self.Builder_1.setGeometry(QtCore.QRect(20, 60, 25, 25))
         self.Builder_1.setStyleSheet("Background: rgb(255, 0, 0)\n"
                                      "")
         self.Builder_1.setAlignment(QtCore.Qt.AlignCenter)
         self.Builder_1.setObjectName("Builder_1")
-        self.Builder_2 = QtWidgets.QLabel(self.Builder_Group)
+        self.Builder_2 = Ui_MainWindow.ClickLabel(self.Builder_Group)
         self.Builder_2.setGeometry(QtCore.QRect(50, 60, 25, 25))
         self.Builder_2.setStyleSheet("Background: rgb(0, 0, 255)\n"
                                      "")
         self.Builder_2.setAlignment(QtCore.Qt.AlignCenter)
         self.Builder_2.setObjectName("Builder_2")
-        self.Builder_3 = QtWidgets.QLabel(self.Builder_Group)
+        self.Builder_3 = Ui_MainWindow.ClickLabel(self.Builder_Group)
         self.Builder_3.setGeometry(QtCore.QRect(80, 60, 25, 25))
         self.Builder_3.setStyleSheet("Background: rgb(255, 255, 0)\n"
                                      "")
         self.Builder_3.setAlignment(QtCore.Qt.AlignCenter)
         self.Builder_3.setObjectName("Builder_3")
-        self.Builder_4 = QtWidgets.QLabel(self.Builder_Group)
+        self.Builder_4 = Ui_MainWindow.ClickLabel(self.Builder_Group)
         self.Builder_4.setGeometry(QtCore.QRect(110, 60, 25, 25))
         self.Builder_4.setStyleSheet("Background: rgb(0, 255, 0)\n"
                                      "")
         self.Builder_4.setAlignment(QtCore.Qt.AlignCenter)
         self.Builder_4.setObjectName("Builder_4")
-        self.Builder_5 = QtWidgets.QLabel(self.Builder_Group)
+        self.Builder_5 = Ui_MainWindow.ClickLabel(self.Builder_Group)
         self.Builder_5.setGeometry(QtCore.QRect(140, 60, 25, 25))
         self.Builder_5.setStyleSheet("Background: rgb(170, 0, 255)\n"
                                      "")
         self.Builder_5.setAlignment(QtCore.Qt.AlignCenter)
         self.Builder_5.setObjectName("Builder_5")
-        self.Builder_6 = QtWidgets.QLabel(self.Builder_Group)
+        self.Builder_6 = Ui_MainWindow.ClickLabel(self.Builder_Group)
         self.Builder_6.setGeometry(QtCore.QRect(170, 60, 25, 25))
         self.Builder_6.setStyleSheet("Background: rgb(255, 85, 0)\n"
                                      "")
         self.Builder_6.setAlignment(QtCore.Qt.AlignCenter)
         self.Builder_6.setObjectName("Builder_6")
-        self.Builder_7 = QtWidgets.QLabel(self.Builder_Group)
+        self.Builder_7 = Ui_MainWindow.ClickLabel(self.Builder_Group)
         self.Builder_7.setGeometry(QtCore.QRect(200, 60, 25, 25))
         self.Builder_7.setStyleSheet("Background: rgb(255, 85, 127)\n"
                                      "")
         self.Builder_7.setAlignment(QtCore.Qt.AlignCenter)
         self.Builder_7.setObjectName("Builder_7")
-        self.Builder_8 = QtWidgets.QLabel(self.Builder_Group)
+        self.Builder_8 = Ui_MainWindow.ClickLabel(self.Builder_Group)
         self.Builder_8.setGeometry(QtCore.QRect(230, 60, 25, 25))
         self.Builder_8.setStyleSheet("Background: rgb(170, 170, 127)\n"
                                      "")
         self.Builder_8.setAlignment(QtCore.Qt.AlignCenter)
         self.Builder_8.setObjectName("Builder_8")
-        self.Builder_9 = QtWidgets.QLabel(self.Builder_Group)
+        self.Builder_9 = Ui_MainWindow.ClickLabel(self.Builder_Group)
         self.Builder_9.setGeometry(QtCore.QRect(260, 60, 25, 25))
         self.Builder_9.setStyleSheet("Background: rgb(255, 255, 255)\n"
                                      "")
         self.Builder_9.setAlignment(QtCore.Qt.AlignCenter)
         self.Builder_9.setObjectName("Builder_9")
         self.Main_Label = QtWidgets.QLabel(self.centralwidget)
-        self.Main_Label.setGeometry(QtCore.QRect(10, 10, 299, 299))
+        self.Main_Label.setGeometry(QtCore.QRect(10, 10, 300, 300))
         self.Main_Label.setText("")
         self.Main_Label.setPixmap(QtGui.QPixmap("/Blank.bmp"))
         self.Main_Label.setObjectName("Main_Label")
@@ -104,15 +116,15 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.Nav_Up = QtWidgets.QPushButton(self.Nav_Group)
         self.Nav_Up.setGeometry(QtCore.QRect(70, 26, 25, 25))
         self.Nav_Up.setObjectName("Nav_Up")
-        self.Left_Right = QtWidgets.QPushButton(self.Nav_Group)
-        self.Left_Right.setGeometry(QtCore.QRect(100, 56, 25, 25))
-        self.Left_Right.setObjectName("Left_Right")
+        self.Nav_Right = QtWidgets.QPushButton(self.Nav_Group)
+        self.Nav_Right.setGeometry(QtCore.QRect(100, 56, 25, 25))
+        self.Nav_Right.setObjectName("Nav_Right")
         self.Nav_Left = QtWidgets.QPushButton(self.Nav_Group)
         self.Nav_Left.setGeometry(QtCore.QRect(40, 56, 25, 25))
         self.Nav_Left.setObjectName("Nav_Left")
-        self.Left_Down = QtWidgets.QPushButton(self.Nav_Group)
-        self.Left_Down.setGeometry(QtCore.QRect(70, 86, 25, 25))
-        self.Left_Down.setObjectName("Left_Down")
+        self.Nav_Down = QtWidgets.QPushButton(self.Nav_Group)
+        self.Nav_Down.setGeometry(QtCore.QRect(70, 86, 25, 25))
+        self.Nav_Down.setObjectName("Nav_Down")
         self.Nav_X_Line = QtWidgets.QLineEdit(self.Nav_Group)
         self.Nav_X_Line.setGeometry(QtCore.QRect(178, 36, 31, 20))
         self.Nav_X_Line.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
@@ -144,35 +156,101 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        #Connections
+        self.Nav_Up.clicked.connect(self.MoveUp)
+        self.Nav_Down.clicked.connect(self.MoveDown)
+        self.Nav_Left.clicked.connect(self.MoveLeft)
+        self.Nav_Right.clicked.connect(self.MoveRight)
+        self.Nav_Centre.clicked.connect(self.MoveCentre)
+        self.Nav_Select.clicked.connect(self.MoveSelect)
+        self.Builder_1.clicked.connect(partial(self.ChangeColour, self.Builder_1))
+        self.Builder_2.clicked.connect(partial(self.ChangeColour, self.Builder_2))
+        self.Builder_3.clicked.connect(partial(self.ChangeColour, self.Builder_3))
+        self.Builder_4.clicked.connect(partial(self.ChangeColour, self.Builder_4))
+        self.Builder_5.clicked.connect(partial(self.ChangeColour, self.Builder_5))
+        self.Builder_6.clicked.connect(partial(self.ChangeColour, self.Builder_6))
+        self.Builder_7.clicked.connect(partial(self.ChangeColour, self.Builder_7))
+        self.Builder_8.clicked.connect(partial(self.ChangeColour, self.Builder_8))
+        self.Builder_9.clicked.connect(partial(self.ChangeColour, self.Builder_9))
 
-        #Functional Code
-        m = cv2.imread("Blank.bmp")
-        h, w, bpp = numpy.shape(m)
+    def ReadMatrix(self):
+        global m
+        global CoreMat
 
-        print("yo")
-        print("width", str(w))
-        print("height", str(h))
-        print("bpp", str(bpp))
+        for x in range(100):
+            for y in range(100):
+                for x2 in range((x*3)+1-1,(x*3)+1+2):
+                    for y2 in range((y*3)+1-1,(y*3)+1+2):
+                        m[x2][y2] = CoreMat[x][y]
 
-        print(m[1][1])
+    def MoveUp(self):
+        self.Nav_X_Line.setText(str(int(self.Nav_X_Line.text())+1))
+        x = int(self.Nav_X_Line.text())*3+1
+        y = int(self.Nav_Y_Line.text())*3+1
+        self.DrawCursor(x,y)
 
-        m[20][20][0] = 100
-        m[20][20][1] = 100
-        m[20][20][2] = 100
+    def MoveDown(self):
+        self.Nav_X_Line.setText(str(int(self.Nav_X_Line.text())-1))
+        x = int(self.Nav_X_Line.text())*3+1
+        y = int(self.Nav_Y_Line.text())*3+1
+        self.DrawCursor(x,y)
 
-        cv2.waitKey(0)
+    def MoveLeft(self):
+        self.Nav_Y_Line.setText(str(int(self.Nav_Y_Line.text())-1))
+        x = int(self.Nav_X_Line.text())*3+1
+        y = int(self.Nav_Y_Line.text())*3+1
+        self.DrawCursor(x,y)
+
+    def MoveRight(self):
+        self.Nav_Y_Line.setText(str(int(self.Nav_Y_Line.text())+1))
+        x = int(self.Nav_X_Line.text())*3+1
+        y = int(self.Nav_Y_Line.text())*3+1
+        self.DrawCursor(x,y)
+
+    def MoveCentre(self):
+        self.Nav_Y_Line.setText(str(50))
+        self.Nav_X_Line.setText(str(50))
+        self.DrawCursor(151,151)
+
+    def MoveSelect(self):
+        x = int(self.Nav_X_Line.text())*3+1
+        y = int(self.Nav_Y_Line.text())*3+1
+        self.DrawCursor(x,y)
+
+    def DrawCursor(self,x,y):
+
+        self.ReadMatrix()
+
+        for x2 in range(x-2,x+3):
+            for y2 in range(y-2,y+3):
+                if not ((x2 == x-1 or x2 == x or x2 == x+1) and (y2 == y-1 or y2 == y or y2 == y+1)):
+                    m[x2][y2][0] = 100
+                    m[x2][y2][1] = 100
+                    m[x2][y2][2] = 100
+
+
+        cv2.waitKey(10)
 
         im = Image.fromarray(m)
         im.save("Blank2.bmp")
 
         self.Main_Label.setPixmap(QtGui.QPixmap("./Blank2.bmp"))
 
+    def ChangeColour(self, sourcelabel):
+        print(sourcelabel.text())
+
+        color = QtWidgets.QColorDialog.getColor()
+        print(color.name())
+        sourcelabel.setStyleSheet("Background: "+color.name()+"\n")
+
+        print(CurrColour)
+
     #Also QtDesigner Output
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.Builder_Group.setTitle(_translate("MainWindow", "Sandpile Builder"))
-        self.Builder_Label.setText(_translate("MainWindow", "Sandpile Limit (1-9)"))
+        self.Builder_Label.setText(_translate("MainWindow", "Sandpile Limit (3-9)"))
         self.Builder_Line.setText(_translate("MainWindow", "4"))
         self.Builder_1.setText(_translate("MainWindow", "1"))
         self.Builder_2.setText(_translate("MainWindow", "2"))
@@ -186,9 +264,9 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.Nav_Group.setTitle(_translate("MainWindow", "Navigation"))
         self.Nav_Centre.setText(_translate("MainWindow", "C"))
         self.Nav_Up.setText(_translate("MainWindow", "U"))
-        self.Left_Right.setText(_translate("MainWindow", "R"))
+        self.Nav_Right.setText(_translate("MainWindow", "R"))
         self.Nav_Left.setText(_translate("MainWindow", "L"))
-        self.Left_Down.setText(_translate("MainWindow", "D"))
+        self.Nav_Down.setText(_translate("MainWindow", "D"))
         self.Nav_X_Line.setText(_translate("MainWindow", "50"))
         self.Nav_Y_Line.setText(_translate("MainWindow", "50"))
         self.Nav_X_Lab.setText(_translate("MainWindow", "X"))
@@ -198,6 +276,24 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.Control_Begin.setText(_translate("MainWindow", "Begin!"))
         self.Control_Clear.setText(_translate("MainWindow", "Clear"))
         self.show()
+
+        #Initial Globals
+        global CurrColour
+        global m
+        global CoreMat
+        CurrColour = 1
+
+        #Core 100x100 Matrix
+        CoreMat = [[(0,0,0)]*100 for i in range(100)]
+
+        #Functional Code
+        m = cv2.imread("Blank.bmp")
+
+        self.ReadMatrix()
+        im = Image.fromarray(m)
+        im.save("Blank2.bmp")
+
+        self.Main_Label.setPixmap(QtGui.QPixmap("./Blank2.bmp"))
 
 
 if __name__ == '__main__':
